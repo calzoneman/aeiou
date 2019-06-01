260e9963599595cb559d67b69d00cf0f1d11ef80
Calvin Montgomery <calzoneman@gmail.com>
2019-06-01 13:24:11 -0700

    Add uncaught exception logger

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 var Webserver = require('./lib/webserver');
 var config = require('./config.json');
+var winston = require('winston');
 
-new Webserver(config);
+let logger = new winston.Logger({
+    level: !!process.env.DEBUG ? 'debug' : 'info',
+    transports: [
+        new (winston.transports.Console)({
+            colorize: true
+        }),
+        new (winston.transports.File)({
+            filename: 'aeiou.log',
+            json: false
+        })
+    ]
+});
+
+process.on('uncaughtException', error => {
+    logger.error(`Uncaught exception: ${error.stack}`);
+});
+
+new Webserver(config, logger);
